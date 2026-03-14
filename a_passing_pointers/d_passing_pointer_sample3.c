8e7ab7e4ac9ba1393d1f303eae1ac705a9876908
jed jarin <jedjarin@gmail.com>
2026-03-14 21:32:55 +0900

    info for gaming

--- a_passing_pointers/d_passing_pointer_sample3.c
+++ a_passing_pointers/d_passing_pointer_sample3.c
@@ -17,3 +17,21 @@ void attack(int *enemy_health)
 {
     *enemy_health -= 30;
 }
+/*
+void take_damage(int *health)
+void heal(int *health)
+void gain_xp(int *xp)
+void level_up(int *level)
+
+update position
+change health
+change score
+inventory
+stats
+
+dog_health
+dog_stamina
+dog_level
+dog_xp
+
+*/
