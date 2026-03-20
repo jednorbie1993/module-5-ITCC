e4af23f3aa14c9fc0fa91933b50aa91cc26c33d4
jed jarin <jedjarin@gmail.com>
2026-03-20 22:59:16 +0900

    debug

---
+++ s_debugging_using_if/b_debug.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#define DEBUG 0// palitan mo lang ng 1 and 0 processor directives parin to 
+
+int main() {
+
+#if DEBUG == 1
+    printf("Debug Mode ON\n");
+#endif
+
+    printf("Program Running...\n");
+
+    return 0;
+}
+
+/*
+reviewed
+
+march 20, 2026
+
+*/
