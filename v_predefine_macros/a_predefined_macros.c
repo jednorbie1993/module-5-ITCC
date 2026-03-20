2eabfa2f551226a5af61314e91140b0fcc8eb8d8
jed jarin <jedjarin@gmail.com>
2026-03-20 23:42:01 +0900

    added logic

--- v_predefine_macros/a_predefined_macros.c
+++ v_predefine_macros/a_predefined_macros.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 int main() {
-    printf("File: %s\n", __FILE__);
-    printf("Line: %d\n", __LINE__);
-    printf("Date: %s\n", __DATE__);
-    printf("Time: %s\n", __TIME__);
+    printf("File name: %s\n", __FILE__);
+    printf("Line now: %d\n", __LINE__);
+    printf("Date now: %s\n", __DATE__);
+    printf("Time now: %s\n", __TIME__);
     return 0;
 }
 
